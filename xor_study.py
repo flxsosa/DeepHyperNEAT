e603786d1f6ed5f393b7703ae035cf0b7eb00345
twizzler <twizzler@dhcp-18-189-47-107.dyn.MIT.EDU>
2018-03-18 17:20:19 -0400

    changed sheet dims

--- xor_study.py
+++ xor_study.py
@@ -6,7 +6,7 @@ from decode import decode
 from visualize import draw_net
 
 sub_in_dims = [1,2]
-sub_sh_dims = [1,3]
+sub_sh_dims = [1,1]
 sub_o_dims = 1
 xor_inputs = [(0.0,0.0),(0.0,1.0),(1.0,0.0),(1.0,1.0)]
 expected_outputs = [0.0, 1.0, 1.0, 0.0]
@@ -22,7 +22,7 @@ def xor(genomes):
 		substrate = decode(cppn,sub_in_dims,sub_o_dims,sub_sh_dims)
 		sum_square_error = 0.0
 		for inputs, expected in zip(xor_inputs, expected_outputs):
-			inputs = inputs #+ (1.0,)
+			inputs = inputs# + (1.0,)
 			actual_output = substrate.activate(inputs)[0]
 			sum_square_error += ((actual_output - expected)**2.0)/4.0
 		genome.fitness = 1.0 - sum_square_error
@@ -35,13 +35,15 @@ substrate = decode(cppn,sub_in_dims,sub_o_dims,sub_sh_dims)
 
 print("\nChampion Genome: {} with Fitness {}\n".format(winner_genome.key, 
 	  winner_genome.fitness))
-
+print("Ancestry:")
+for anc in winner_genome.ancestors:
+	print(anc)
 cppn = CPPN.create(winner_genome)
 substrate = decode(cppn,sub_in_dims,sub_o_dims,sub_sh_dims)
 sum_square_error = 0.0
 for inputs, expected in zip(xor_inputs, expected_outputs):
 	print("Input: {}\nExpected Output: {}".format(inputs,expected))
-	inputs = inputs #+ (1.0,)
+	inputs = inputs# + (1.0,)
 	actual_output = substrate.activate(inputs)[0]
 	sum_square_error += ((actual_output - expected)**2.0)/4.0
 	print("Actual Output: {}\nLoss: {}".format(actual_output,sum_square_error))
