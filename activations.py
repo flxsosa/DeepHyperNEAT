2a475ae56de0e23343d3c3a723b4bf8b86da265d
twizzler <twizzler@twizzlers-MacBook-Pro.local>
2018-04-23 21:14:04 -0400

    added softmax

--- activations.py
+++ activations.py
@@ -6,7 +6,12 @@ and code for adding new user-defined ones
 from __future__ import division
 import math
 import types
+import numpy as np
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
 def sigmoid_activation(z):
     '''
