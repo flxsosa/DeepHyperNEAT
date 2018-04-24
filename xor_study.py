5a3ee8c9b020b059cbbb5c4609868b9dcc0219ec
twizzler <twizzler@twizzlers-MacBook-Pro.local>
2018-04-23 21:18:08 -0400

    cleaned up

--- xor_study.py
+++ xor_study.py
@@ -29,26 +29,6 @@ def xor(genomes):
 
 # Gather winning and max complexity genome
 winner_genome = pop.run_with_speciation(xor,num_generations)
-max_genome = pop.max_complex_genome
-output_file = open("reports/generation_output.txt", "w")
-for n,g in winner_genome.prev_genomes:
-	c = CPPN.create(g)
-	s = decode(c, sub_in_dims, sub_o_dims, sub_sh_dims)
-	sum_square_error = 0.0
-	for inputs, expected in zip(xor_inputs, expected_outputs):
-		print("Genome From Generation {}".format(n))
-		output_file.write("Genome From Generation {}\n".format(n))
-		print("Input: {}\nExpected Output: {}".format(inputs,expected))
-		output_file.write("Input: {}\nExpected Output: {}\n".format(inputs,expected))
-		inputs = inputs + (0.0,)
-		actual_output = s.activate(inputs)[0]
-		sum_square_error += ((actual_output - expected)**2.0)/4.0
-		print("Actual Output: {}\nLoss: {}\n".format(actual_output,sum_square_error))
-		output_file.write("Actual Output: {}\nLoss: {}\n".format(actual_output,sum_square_error))
-	print("Total Loss: {}".format(sum_square_error))
-	output_file.write("Total Loss: {}\n\n".format(sum_square_error))
-	draw_net(c, filename="reports/champion_images/{}_cppn".format(n))
-	draw_net(s, filename="reports/champion_images/{}_substrate".format(n))
 
 # Decode winner into CPPN and Substrate
 cppn = CPPN.create(winner_genome)
@@ -56,13 +36,6 @@ substrate = decode(cppn,sub_in_dims,sub_o_dims,sub_sh_dims)
 report_ancestry(winner_genome)
 draw_net(cppn, filename="reports/champion_images/xor_cppn")
 draw_net(substrate, filename="reports/champion_images/xor_substrate")
-
-# Decode max complexity genome into CPPN and Substrate
-max_cppn = CPPN.create(max_genome)
-max_substrate = decode(max_cppn,sub_in_dims,sub_o_dims,sub_sh_dims)
-report_ancestry(max_genome, "complex")
-draw_net(max_cppn, filename="reports/complex_images/xor_max_cppn")
-draw_net(max_substrate, filename="reports/complex_images/xor_max_substrate")
 
 print("\nChampion Genome: {} with Fitness {}\n".format(winner_genome.key, 
 	  											winner_genome.fitness))
